9150cb4f4bfb1646545759f12ff651a7443c5047
Essi Salmenkivi <essi.salmenkivi@helsinki.fi>
2015-01-31 22:00:45 +0200

    creates markers and text

--- scripts.js
+++ scripts.js
@@ -1,8 +1,13 @@
+
+
 $(window).load(function() {
+    var position = new google.maps.LatLng(60.671, 24.938);
+
     var mapOptions = {        //set up the map
-        center: { lat: 60.671, lng: 24.938},
+        center: position,
         zoom: 8
     };
+
     var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
     var ylenews;   //download news 
@@ -13,6 +18,20 @@ $(window).load(function() {
              {
                  $("#news").append(ylenews[i].title);
                  $("#news").append("<p>");
+
+                 var offset = new google.maps.LatLng(60.671, 22.938 + 0.5*i);
+
+                 var marker = new google.maps.Marker({
+                     position: offset,
+                     map: map
+                 });
+
+                 var info = new google.maps.InfoWindow({
+                     content: ylenews[i].title
+                 });
+
+                 info.open(map, marker);
+
              }
              console.log(ylenews[0].description);
              //document.getElementById("news").appendChild(ylenews);
